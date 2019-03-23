dd82861633734886cc8eda617ebdc16c624b410d
yufrances90 <yufrances90@gmail.com>
2019-03-23 04:35:37 -0400

    feat: Enabled the ability for capturing  images and videos

--- eco/src/camera.page.js
+++ eco/src/camera.page.js
@@ -4,11 +4,36 @@ import { Camera, Permissions } from 'expo';
 
 import styles from './styles';
 
+import Toolbar from './toolbar.component';
+
 export default class CameraPage extends React.Component {
     camera = null;
 
     state = {
+        captures: [],
+        flashMode: null,
+        capturing: null,
+        cameraType: null,
         hasCameraPermission: null,
+    };
+
+    setFlashMode = (flashMode) => this.setState({ flashMode });
+    setCameraType = (cameraType) => this.setState({ cameraType });
+    handleCaptureIn = () => this.setState({ capturing: true });
+
+    handleCaptureOut = () => {
+        if (this.state.capturing)
+            this.camera.stopRecording();
+    };
+
+    handleShortCapture = async () => {
+        const photoData = await this.camera.takePictureAsync();
+        this.setState({ capturing: false, captures: [photoData, ...this.state.captures] })
+    };
+
+    handleLongCapture = async () => {
+        const videoData = await this.camera.recordAsync();
+        this.setState({ capturing: false, captures: [videoData, ...this.state.captures] });
     };
 
     async componentDidMount() {
@@ -20,8 +45,9 @@ export default class CameraPage extends React.Component {
     };
 
     render() {
-        const { hasCameraPermission } = this.state;
 
+        const { hasCameraPermission, flashMode, cameraType, capturing } = this.state;
+        
         if (hasCameraPermission === null) {
             return <View />;
         } else if (hasCameraPermission === false) {
@@ -29,12 +55,25 @@ export default class CameraPage extends React.Component {
         }
 
         return (
-            <View>
+            <React.Fragment>
+                <View>
                 <Camera
                     style={styles.preview}
                     ref={camera => this.camera = camera}
                 />
             </View>
+                <Toolbar 
+                    capturing={capturing}
+                    flashMode={flashMode}
+                    cameraType={cameraType}
+                    setFlashMode={this.setFlashMode}
+                    setCameraType={this.setCameraType}
+                    onCaptureIn={this.handleCaptureIn}
+                    onCaptureOut={this.handleCaptureOut}
+                    onLongCapture={this.handleLongCapture}
+                    onShortCapture={this.handleShortCapture}
+                />
+            </React.Fragment>
         );
     };
 
